73004ed608497105ff8efbeb18f610a9d9bee26d
GACKT-chan <gakuto.chan.san@gmail.com>
2020-12-17 00:08:28 +0900

    テスト

--- listToCSV.py
+++ listToCSV.py
@@ -1,6 +1,5 @@
 #!/usr/local/bin/python3
 # coding: utf-8
-import numpy as np
 print("inputFileName OutputFileName")
 inName,makeName = input().split(" ")
 with open(inName,'r',encoding="utf-16-le") as f:
